02702b7de06bd6c04f944b7e6aa504651b11fd1f
Juandavidsilva-hub <juan_david.silva@uao.edu.co>
2025-11-20 23:20:41 -0500

    2

---
+++ Assets/4_Silva/Scripts/luces.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Light[] luces;
+    public void ApagarLuces()
+    {
+        for (int i = 0; i < luces.Length; i++)
+        {
+            if (luces[i] != null && luces[i].enabled)
+            {
+                luces[i].enabled = false;
+            }
+        }
+
+    }
+    public void EncenderLuces()
+    {
+        for (int i = 0; i < luces.Length; i++)
+        {
+            if (luces[i] != null && luces[i].enabled)
+            {
+                luces[i].enabled = true;
+            }
+        }
+
+    }
+}
+
